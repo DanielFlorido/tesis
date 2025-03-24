068d8d25c9a0caa107953292bff3ade03464abc2
Daniel Florido <d.florido@javeriana.edu.co>
2025-03-23 21:08:52 -0500

    Initialize Spring Boot project with basic structure and configuration

---
+++ src/main/java/com/javeriana/tesis/TesisApplication.java
@@ -0,0 +1,13 @@
+package com.javeriana.tesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javeriana/tesis/TesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javeriana.tesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
